70ece5ed4166f32a60d6a3ea94dbe3cefe51196b
Roma Koulikov <rkoulikov@pm.me>
2023-11-24 11:59:19 -0500

    Modified first dl model py file (ch 2) to coincide with new flash syntax

--- chapter02/first_dl_with_mlflow.py
+++ chapter02/first_dl_with_mlflow.py
@@ -4,14 +4,14 @@ import torch
 from flash.core.data.utils import download_data
 from flash.text import TextClassificationData, TextClassifier
 
-download_data("https://pl-flash-data.s3.amazonaws.com/imdb.zip", "./data/")
+#download_data("https://pl-flash-data.s3.amazonaws.com/imdb.zip", "./data/")
 datamodule = TextClassificationData.from_csv(
-    input_fields="review",
+    input_field="review",
     target_fields="sentiment",
     train_file="data/imdb/train.csv",
     val_file="data/imdb/valid.csv",
-    test_file="data/imdb/test.csv"
-)
+    test_file="data/imdb/test.csv",
+    batch_size=3)
 
 classifier_model = TextClassifier(backbone="prajjwal1/bert-tiny", num_classes=datamodule.num_classes)
 trainer = flash.Trainer(max_epochs=3, gpus=torch.cuda.device_count())
@@ -25,4 +25,4 @@ mlflow.pytorch.autolog()
 
 with mlflow.start_run(experiment_id=experiment.experiment_id, run_name="chapter02"):
     trainer.finetune(classifier_model, datamodule=datamodule, strategy="freeze")
-    trainer.test()
+    trainer.test(datamodule=datamodule)
